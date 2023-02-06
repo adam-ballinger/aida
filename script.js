a20b86b0f0788a2ae53d505ea79fc74e660091db
adam-ballinger <78917961+adam-ballinger@users.noreply.github.com>
2023-02-05 18:52:02 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -2,18 +2,42 @@ var hero_word = ""
 let history = []
 var score = 0
 var special = {
-  'ADAM': {'emoji': '👨', 'message': 'David Adam Ballinger!'},
+  'ADAM': {'emoji': '🧔', 'message': 'David Adam Ballinger!'},
   'AIDA': {'emoji': '🦄', 'message': 'Aida Marie Heller Ballinger!'},
   'MOE': {'emoji': '🐶', 'message': 'Mozart "Moe" Ballinger!'},
-  'MOM': {'emoji': '🤰', 'message': 'We love mom'},
+  'ANNA': {'emoji': '🤰', 'message': 'We love mom.'},
+  'DONNA': {'emoji': '👩', 'message': 'I love you, Grandma Donna. -Aida'}
 }
+
+var milestones = [
+  {'score': 50, 'message': '50 points! Keep it up! Earn 100 points for more letters', 'function': () => {}},
+  {'score': 100, 'message': "100 points! You're ready for more letters.", 'function': () => {
+    $('#last_row').after(`
+    <div id="new_row" class="row mb-3 text-center">
+      <div class="col-4 themed-grid-col">
+        <button id="button_7" onclick="button_click(this.id)" class="btn w-100 h-100 rounded-4 shadow-lg"><p class="display-1 fw-bold">G</p></button>
+      </div>
+      <div class="col-4 themed-grid-col">
+        <button id="button_8" onclick="button_click(this.id)" class="btn w-100 h-100 rounded-4 shadow-lg"><p class="display-1 fw-bold">N</p></button>
+      </div>
+      <div class="col-4 themed-grid-col">
+        <button id="button_9" onclick="button_click(this.id)" class="btn w-100 h-100 rounded-4 shadow-lg"><p class="display-1 fw-bold">R</p></button>
+      </div>
+    </div>
+    `)
+  }},
+  {'score': 150, 'message': '150 points! Great work! Earn 300 points for a medal.', 'function': () => {}},
+  {'score': 300, 'message': '300 points! You get the medal! 🏅', 'function': () => {
+    $('#header').text("🦄AIDA'S WEBSITE🏅")
+  }}
+]
+
+var milestone = 0
 
 function button_click(button_id){
   
   var letter = $("#" + button_id + " p").text()
-  console.log("Cliked " + button_id + ", got letter " + letter)
   hero_word = hero_word + letter
-  console.log("Updated word: " + hero_word)
   $('#hero_message').text(hero_word)
   if(hero_word.length > 2) {
     if(check_history(hero_word)) {
@@ -44,6 +68,11 @@ function score_points(word) {
   $('#message').html('<h6>' + word + '</h6>')
   hero_word = ''
   $("#clear").attr('disabled', true)
+  if(score >= milestones[milestone].score) {
+    $('#hero_message').text(milestones[milestone].message)
+    milestones[milestone].function()
+    milestone++
+  }
 }
 
 function check_dictionary(word) {
